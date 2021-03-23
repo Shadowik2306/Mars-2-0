c458cef35d127ac955ef5d66fb447340d5d9b822
shadowik <zahar.mark@bk.ru>
2021-03-24 00:17:19 +0400

    Top

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from wtforms import StringField, PasswordField, SubmitField, Label
 from wtforms.validators import DataRequired
 import os
 
+from data import db_session
+from data.users import User
+from data.job import Jobs
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -113,37 +117,13 @@ def galery():
             file.write(f)
 
 
-@app.route('/sample_file_upload', methods=['POST', 'GET'])
-def sample_file_upload():
-    if request.method == 'GET':
-        return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                             <link rel="stylesheet"
-                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                             crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример загрузки файла</title>
-                          </head>
-                          <body>
-                            <h1>Загрузим файл</h1>
-                            <form method="post" enctype="multipart/form-data">
-                               <div class="form-group">
-                                    <label for="photo">Выберите файл</label>
-                                    <input type="file" class="form-control-file" id="photo" name="file">
-                                </div>
-                                <button type="submit" class="btn btn-primary">Отправить</button>
-                            </form>
-                          </body>
-                        </html>'''
-    elif request.method == 'POST':
-        f = request.files['file']
-        with open(str(os.listdir('static/img/galery')) +'.jpg', 'w') as file:
-            file.write(f)
-        return "Форма отправлена"
+@app.route('/')
+def works_log():
+    db_session.global_init('db/blogs.db')
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(type(jobs[1].team_leader))
+    return render_template('works_log.html', jobs=jobs)
 
 if __name__ == '__main__':
     app.run(port=8081, host='127.0.0.1')
