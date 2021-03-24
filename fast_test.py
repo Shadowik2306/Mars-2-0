741225fa3d54cebf0bf98a906d65d9ab96689d5d
shadowik <zahar.mark@bk.ru>
2021-03-24 17:57:56 +0400

    GOGOGOGo

--- fast_test.py
+++ fast_test.py
@@ -1,11 +1,10 @@
 from data.job import Jobs
 from data import db_session
 from data.users import User
+from data.department import Department
 from data.job import Jobs
 
 
 db_session.global_init(input())
 db_sess = db_session.create_session()
-l = max([len(i.collaborators) for i in db_sess.query(Jobs).all()])
-for job in db_sess.query(Jobs).filter(Jobs.collaborators.like('_' * l)).all():
-    print(job.id.name)
+depart = db_sess.query(Department).filter(Department.id == 1).all()
