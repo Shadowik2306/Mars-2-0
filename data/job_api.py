263ad9bf378f5d4f08a5282bb7e85f222bed7710
shadowik <zahar.mark@bk.ru>
2021-03-31 16:35:54 +0400

    FirstApiPost

--- data/job_api.py
+++ data/job_api.py
@@ -37,3 +37,27 @@ def get_one_job(jobs_id):
             'jobs': [jobs.to_dict()]
         }
     )
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_news():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['team_leader', 'user', "job", "work_size",
+                  'collaborators', 'start_date', 'end_date', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    job = Jobs(
+        team_leader=request.json['team_leader'],
+        user=request.json['user'],
+        job=request.json['job'],
+        collaborators=request.json['collaborators'],
+        work_size=request.json['work_size'],
+        start_date=request.json['start_date'],
+        end_date=request.json['end_date'],
+        is_finished=request.json['is_finished'],
+    )
+    db_sess.add(job)
+    db_sess.commit()
+
+    return jsonify({'success': 'Ok'})
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from requests import get
+from requests import get, post
 
 
 print(get('http://127.0.0.1:8080/api/jobs').json())
 print(get('http://127.0.0.1:8080/api/jobs/1').json())
 print(get('http://127.0.0.1:8080/api/jobs/999').json())
 print(get('http://127.0.0.1:8080/api/jobs/helloWorld').json())
+print(post('http://127.0.0.1:8080/api/jobs').json())
