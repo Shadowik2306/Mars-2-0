79222765c919586791548af54dc96459da5da217
shadowik <zahar.mark@bk.ru>
2021-03-31 18:02:52 +0400

    Test

--- test.py
+++ test.py
@@ -9,3 +9,19 @@ print(post('http://127.0.0.1:8082/api/jobs',
         'work_size': 20,
         'collaborators': '1, 2',
         'is_finished': True}).json())
+print(post('http://127.0.0.1:8082/api/jobs',
+    json={
+        'id': 9999,
+        'job': 'Hello Mars',
+        'team_leader': '1',
+        'work_size': 20,
+        'collaborators': '1, 2',
+        'is_finished': True}).json()) #правильное
+print(post('http://127.0.0.1:8082/api/jobs',
+    json={
+        'job': 'Hello Mars',
+        'work_size': 20,
+        'collaborators': '1, 2',
+        'is_finished': True}).json())
+print(post('http://127.0.0.1:8082/api/jobs').json())
+
